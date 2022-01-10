ee48bf12160f7ad1e5c228856e2a2686d06aa6dd
Mukut <87146309+webmukut@users.noreply.github.com>
2022-01-10 09:18:45 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+// $(document).ready(function(){
+//   new VenoBox({
+//     selector: ".showvideo",
+//     autoplay: "true",
+// });
+// });
+ /*Scroll to top when arrow up clicked END*/
+
+ $(document).ready(function(){
+  $('.testimonial').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
+
+
+var btn = $('#backtotop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 100) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
